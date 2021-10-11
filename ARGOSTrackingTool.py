53b5f23c24f85ecac17d385dece739548f258e4c
charlesmuirhead <68345791+charlesmuirhead@users.noreply.github.com>
2021-10-11 16:30:33 -0400

    Task 4b complete

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -5,12 +5,12 @@
 #   the user to view the location of the turtle for a specified
 #   date entered via user input.
 #
-# Author: Charlie Muirhead (charles.muirhead@duke.edu)
+# Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2021
 #--------------------------------------------------------------
 
-#Create a variable pointing to the data file
-file_name = './data/raw/Sara.txt'
+#Create a variable point to the data file
+file_name = './data/raw/sara.txt'
 
 #Create a file object from the file
 file_object = open(file_name,'r')
@@ -21,18 +21,18 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
-#Pretend we read one line of data from the file
-lineString = line_list[100]
+#Iterate through all lines in linelist
+for lineString in line_list:
+    if lineString[0] in ("#",'u'): continue
 
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract the items in list into variables
+    record_id = lineData[0]  # ARGOS tracking record ID
+    obs_date = lineData[2]   # Observation date
+    ob_lc = lineData[4]      # Observation Location Class
+    obs_lat = lineData[6]    # Observation Latitude
+    obs_lon = lineData[7]    # Observation Longitude
+    
+    print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
