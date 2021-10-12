21908a78215e244d8466110cd2988ef7ac4b998d
charlesmuirhead <68345791+charlesmuirhead@users.noreply.github.com>
2021-10-11 20:30:39 -0400

    Task 6 complete

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -39,7 +39,9 @@ for lineString in line_list:
     obs_lat = lineData[6]    # Observation Latitude
     obs_lon = lineData[7]    # Observation Longitude
     
-    print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-    date_dict[record_id] = obs_date
-    coord_dict[record_id] = (obs_lat,obs_lon)
+    if ob_lc in ("1","2","3"):
+    
+        print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat,obs_lon)
 
